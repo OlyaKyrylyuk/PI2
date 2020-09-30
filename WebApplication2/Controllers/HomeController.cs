b67515d7884719bb240ad7396df9e8087e7daf57
OlyaKyrylyuk <olya_kyrylyuk15@ukr.net>
2020-09-30 11:04:12 +0300

    first commit

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+     
+        Lessons[] les = {
+         new Lessons {Lesson_Id = 1, Name_Group = "ІПЗ", All_students=25, Students_attended=18},
+         new Lessons {Lesson_Id = 2, Name_Group = "КС", All_students=30, Students_attended=15},
+         new Lessons {Lesson_Id = 3, Name_Group = "ОМ", All_students=20, Students_attended=12},
+         new Lessons {Lesson_Id = 4, Name_Group = "КН", All_students=22, Students_attended=13},
+     };
+     
+
+
+        public ActionResult Index()
+        {
+            return View(les);
+        }
+    }
+}
---
+++ WebApplication2/Models/Lessons.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Lessons
+    {
+            public int Lesson_Id { get; set; }
+            public string Name_Group { get; set; }
+            public int All_students { get; set; }
+            public int  Students_attended { set; get; }
+        
+    }
+}
